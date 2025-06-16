21c54fbc79118291603191bedc3c7471723b7d60
angcm25 <merari.perdomo@catolica.edu.sv>
2025-06-15 18:12:55 -0600

    feat: integrate Tailwind CSS and update app structure
    - Added Tailwind CSS and PostCSS dependencies to package.json.
    - Refactored app.html to implement a new layout with a navigation bar and greeting message.
    - Updated app.ts to include a toggle function for displaying the greeting message.
    - Modified index.html to ensure proper rendering of the app component.
    - Updated styles.css to import Tailwind CSS for styling.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,               // <--- Agrega esto, que es necesario para standalone
+  imports: [RouterOutlet, NgIf], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'portafolio-merari';
+  nombre = 'Merari';
+  mostrarMensaje = true;
+
+
+toggleMensaje(){
+  this.mostrarMensaje = !this.mostrarMensaje;
+}
 }
